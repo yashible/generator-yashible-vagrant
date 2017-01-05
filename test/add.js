9b27e146fe6d472cfffe39ad326fde65a0611734
Rui Fernando Hayashi <rfhayashi@gmail.com>
2017-01-05 11:28:03 -0200

    refactored add.js test case

--- test/add.js
+++ test/add.js
@@ -10,47 +10,46 @@ describe('generator-yashible-vagrant:add', function () {
   // increase timeout as the code does http calls
   this.timeout(10000);
 
-  var addedRole = 'rfhayashi.openjdk';
+  describe('happy path', function () {
+    var addedRole = 'rfhayashi.openjdk';
 
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/add'))
-      .inTmpDir(function (dir) {
-        fs.mkdir(dir + '/ansible');
-        fs.writeFileSync(dir + '/ansible/requirements.yml', '---\n- src: somerole');
-      })
-      .withArguments([addedRole])
-      .toPromise();
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/add'))
+        .inTmpDir(function (dir) {
+          fs.mkdir(dir + '/ansible');
+          fs.writeFileSync(dir + '/ansible/requirements.yml', '---\n- src: somerole');
+        })
+        .withArguments([addedRole])
+        .toPromise();
+    });
+
+    it('it adds role to requirements.yml', function () {
+      var content = fs.readFileSync('ansible/requirements.yml', 'utf-8');
+      var requirements = yaml.parse(content);
+      assert(_.find(requirements, function (r) {
+        return r.src === addedRole && r.version !== undefined && r.version !== null;
+      }) !== undefined);
+    });
   });
 
-  it('it adds role to requirements.yml', function () {
-    var content = fs.readFileSync('ansible/requirements.yml', 'utf-8');
-    var requirements = yaml.parse(content);
-    assert(_.find(requirements, function (r) {
-      return r.src === addedRole && r.version !== undefined && r.version !== null;
-    }) !== undefined);
-  });
-});
+  describe('edge cases', function () {
+    it('it shows nice error message if added role does not exist', function (done) {
+      helpers.run(path.join(__dirname, '../generators/add'))
+        .inTmpDir(function (dir) {
+          fs.mkdirSync(dir + '/ansible');
+          fs.writeFileSync(dir + '/ansible/requirements.yml', '---');
+        })
+        .withArguments(['user.nonexistent'])
+        .on('error', function (error) {
+          assert.equal(error.message, 'Role user.nonexistent was not found');
 
-describe('generator-yashible-vagrant:add - non existing role', function () {
-  // increase timeout as the code does http calls
-  this.timeout(10000);
+          done();
+        })
+        .on('end', function () {
+          assert.ifError(new Error('it should have thrown an error'));
 
-  it('it shows nice error message if added role does not exist', function (done) {
-    helpers.run(path.join(__dirname, '../generators/add'))
-      .inTmpDir(function (dir) {
-        fs.mkdirSync(dir + '/ansible');
-        fs.writeFileSync(dir + '/ansible/requirements.yml', '---');
-      })
-      .withArguments(['user.nonexistent'])
-      .on('error', function (error) {
-        assert.equal(error.message, 'Role user.nonexistent was not found');
-
-        done();
-      })
-      .on('end', function () {
-        assert.ifError(new Error('it should have thrown an error'));
-
-        done();
-      });
+          done();
+        });
+    });
   });
 });
